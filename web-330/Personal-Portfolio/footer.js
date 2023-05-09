83a48c022eacd7fbc9e83790c14264e23b76ed41
Michael Christman <mchristman@my365.bellevue.edu>
2023-05-09 16:15:16 -0500

    Update footer.js

--- web-330/Personal-Portfolio/footer.js
+++ web-330/Personal-Portfolio/footer.js
@@ -6,11 +6,13 @@ class Footer extends HTMLElement
 
     connectedCallback() {
         this.innerHTML = `
-            <div id="footer">                                   
-                <a href="https://github.com/MichaelChristman1999/MichaelChristman1999.github.io" target="_blank">My GitHub Repository</a>
-                <a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/" target="_blank">Bellevue Web Development Degree</a>
-                <a href="https://www.youtube.com/user/bellevueuniversity" target="_blank">Bellevue University YouTube</a>
-                <a href="https://github.com/buwebdev/web-330" target="_blank">BUWEB-330 GitHub Repository</a> 
+            <div id="footer">   
+            <ul id="footer-bar">
+                <li class="navigation-link"><a href="https://github.com/MichaelChristman1999/MichaelChristman1999.github.io" target="_blank">My GitHub Repository</a></li>
+                <li class="navigation-link"><a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/" target="_blank">Bellevue Web Development Degree</a></li>
+                <li class="navigation-link"><a href="https://www.youtube.com/user/bellevueuniversity" target="_blank">Bellevue University YouTube</a></li>
+                <li class="navigation-link"><a href="https://github.com/buwebdev/web-330" target="_blank">BUWEB-330 GitHub Repository</a></li>
+                </ul>
             </div>
         `;
     }
