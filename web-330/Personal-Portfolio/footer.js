1ad782ed405c7419c47f33f4933f5f7417d8fbb6
Michael Christman <mchristman@my365.bellevue.edu>
2023-05-09 18:17:21 -0500

    Update footer.js

--- web-330/Personal-Portfolio/footer.js
+++ web-330/Personal-Portfolio/footer.js
@@ -8,10 +8,10 @@ class Footer extends HTMLElement
         this.innerHTML = `
             <div id="footer">   
             <ul id="footer-bar">
-                <li class="navigation-link"><a href="https://github.com/MichaelChristman1999/MichaelChristman1999.github.io" target="_blank">My GitHub Repository</a></li>
-                <li class="navigation-link"><a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/" target="_blank">Bellevue Web Development Degree</a></li>
-                <li class="navigation-link"><a href="https://www.youtube.com/user/bellevueuniversity" target="_blank">Bellevue University YouTube</a></li>
-                <li class="navigation-link"><a href="https://github.com/buwebdev/web-330" target="_blank">BUWEB-330 GitHub Repository</a></li>
+                <li class="navigation-link" style="list-style-type: none"><a href="https://github.com/MichaelChristman1999/MichaelChristman1999.github.io" target="_blank">My GitHub Repository</a></li>
+                <li class="navigation-link" style="list-style-type: none"><a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/" target="_blank">Bellevue Web Development Degree</a></li>
+                <li class="navigation-link" style="list-style-type: none"><a href="https://www.youtube.com/user/bellevueuniversity" target="_blank">Bellevue University YouTube</a></li>
+                <li class="navigation-link" style="list-style-type: none"><a href="https://github.com/buwebdev/web-330" target="_blank">BUWEB-330 GitHub Repository</a></li>
                 </ul>
             </div>
         `;
