c272fd55b0338f40bb4bc9b0b104ca2a17224166
Michael Christman <mchristman@my365.bellevue.edu>
2023-05-09 16:18:24 -0500

    Update contact.js

--- web-330/Personal-Portfolio/contact.js
+++ web-330/Personal-Portfolio/contact.js
@@ -10,10 +10,10 @@ class Contact extends HTMLElement
                 <h3>mchristm19@gmail.com</h3>
                 <h3>011-001-1999</h3>
                 <h3>Kenosha, WI</h3>
-                <h3>www.mikobrospersonalblog.com</h3>
+                <h3>www.mikobrospersonalblog.wordpress.com</h3>
             </div>
         `;
     }
 }
 
-customElements.define("contact-component", contact);
+customElements.define("contact-component", Contact);
