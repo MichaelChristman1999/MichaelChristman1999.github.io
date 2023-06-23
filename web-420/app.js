bf277657c17d2b000150b0fe83df547ba0059c07
Michael Christman <mchristman@my365.bellevue.edu>
2023-06-23 12:59:06 -0500

    Update app.js

--- web-420/app.js
+++ web-420/app.js
@@ -3,36 +3,66 @@
     Title: app.js
     Author: Michael Christman
     Date: May 25th, 2023
-    Description: App.js file for the WEB-420 week 1 assignment
+    Description: App.js file for WEB-420 coursework weeks 1-9
 */
 
-//Required NPM modules 
 const express = require('express');
 const http = require('http');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const mongoose = require('mongoose');
-const swaggerUI = require('swagger-ui-express');
-const swaggerJS = require('swagger-jsdoc');
+
+// Creating a new variable named app and assign it to express library.
 const app = express();
 
-//Local server wired
+// Setting the port to 3000.
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log('Application started and listening on PORT ' + PORT);
-});
-app.use(express.urlencoded({ extended: true }));
+
+// Setting the app to use express.json().
 app.use(express.json());
 
+// Setting the app to use url encoded.
+app.use(express.urlencoded({ extended: true }));
 
-//Options and Open API modules with additional API details and methods
+// Setting up the routing for the /api endpoints
+const composersAPI = require('./routes/Hoitenga-composer-routes');
+const personAPI = require('./routes/Hoitenga-person-routes');
+
+app.use('/api', composersAPI);
+app.use('/api', personAPI);
+
+// Connecting to Mongo
+const CONN = 'mongodb+srv://web420_user:s3cr3t@bellevueuniversity.g473hiy.mongodb.net/web420DB';
+
+// Showing Server Connection Messages
+mongoose
+  .connect(CONN)
+  .then(() => {
+    console.log('Connection to MongoDB database was successful');
+  })
+  .catch((err) => {
+    console.log('MongoDB Error: ' + err.message);
+  });
+
+// Defining an object literal with named options.
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'WEB 420 RESTful APIs',
-            version: '1.0.0',
-        },
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'WEB 420 RESTful APIs',
+      version: '1.0.0',
     },
-    apis: ['./routes/*.js'], 
-}
-const openapiSpecifications = swaggerJS(options);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(openapiSpecifications));
+  },
+  apis: ['./routes/*.js'], // Files containing annotations for the OpenAPI specification.
+};
+
+// Creating a new variable name openapiSpecification & calling the swaggerJsdoc library using the options object literal.
+const openapiSpecification = swaggerJsdoc(options);
+
+// Wiring openapiSpecification variable to app variable.
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+// Start the server and make it listen on port 3000.
+app.listen(PORT, () => {
+  console.log(`Application started and listening on port ${PORT}`);
+});
